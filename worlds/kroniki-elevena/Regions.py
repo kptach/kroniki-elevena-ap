8300e0be03989fe933453b627bf4266ed483804f
Krystian Ptach-Żurakowski <zielonookikrystian@gmail.com>
2025-06-20 14:58:12 +0200

    Add more

--- worlds/kroniki-elevena/Regions.py
+++ worlds/kroniki-elevena/Regions.py
@@ -51,8 +51,11 @@ def create_regions(multiworld: MultiWorld, player: int):
         
         #"Droga Zachodnia(1)":                           KronikiElevenaRegionData([]),
         #"Droga Zachodnia(2)":                           KronikiElevenaRegionData([]),
-        "Droga połódniowa":                             KronikiElevenaRegionData(["Droga Południowa - Walka"]),
-        "Droga połódniowa cd.":                         KronikiElevenaRegionData([]),
+        "Droga połódniowa":                             KronikiElevenaRegionData(["Droga Południowa - Walka",
+                                                                                  "Sekret na prawo - Złoty Muchomor"]),
+        
+        "Droga połódniowa cd.":                         KronikiElevenaRegionData(["Droga Południowa cd. Dom2 - Temmie",
+                                                                                  "Droga Południowa cd. Dom2 - Armor"]),
         
         "Droga zachodnia(1)":                           KronikiElevenaRegionData(["Droga zachodnia 1 - Walka"]),
         "Droga zachodnia(2)":                           KronikiElevenaRegionData([]),
